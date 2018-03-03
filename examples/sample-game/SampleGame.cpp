103ee9ff25b37fc6c4caeb8519ded12ab15b6300
Filip Smola <smola.filip@hotmail.com>
2018-03-03 14:41:01 +0000

    Add window info to debug GUI

--- examples/sample-game/SampleGame.cpp
+++ examples/sample-game/SampleGame.cpp
@@ -62,8 +62,6 @@ int main() {
 
     // ImGui test data
     bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Loop until the user closes the window
     while (!window::should_close()) {
@@ -75,36 +73,32 @@ int main() {
             // Prepare new frame
             imgui::new_frame();
 
-            // ImGui test
+            // Additional window open flags
+            static bool show_window_debug = false;
+
+            // System stats
             {
-                static float f = 0.0f;
-                static int counter = 0;
-                ImGui::Text(
-                        "Hello, world!");                           // Display some text (you can use a format string too)
-                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-                ImGui::ColorEdit3("clear color", (float *) &clear_color); // Edit 3 floats representing a color
+                ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_Once);
+                ImGui::Begin("System Statistics");
 
-                ImGui::Checkbox("Demo Window",
-                                &show_demo_window);      // Edit bools storing our windows open/close state
-                ImGui::Checkbox("Another Window", &show_another_window);
-
-                if (ImGui::Button(
-                        "Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                    counter++;
-                ImGui::SameLine();
-                ImGui::Text("counter = %d", counter);
-
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate,
+                ImGui::Text("Average %.3f ms/frame (%.1f FPS)",
+                            1000.0f / ImGui::GetIO().Framerate,
                             ImGui::GetIO().Framerate);
-            }
-            if (show_another_window) {
-                ImGui::Begin("Another Window", &show_another_window);
-                ImGui::Text("Hello from another window!");
-                if (ImGui::Button("Close Me"))
-                    show_another_window = false;
+
+                if (ImGui::CollapsingHeader("Additional windows:")) {
+                    ImGui::Checkbox("Window info", &show_window_debug);
+                }
+
+
                 ImGui::End();
             }
-            if (show_demo_window) {
+
+            // Window info
+            if (show_window_debug)
+                window::show_debug();
+
+            // Demo window
+            {
                 ImGui::SetNextWindowPos(ImVec2(650, 20),
                                         ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
                 ImGui::ShowDemoWindow(&show_demo_window);

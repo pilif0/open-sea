0b9013e6b7d07867b79a6026fe4ca9504f30e0dd
Filip Smola <smola.filip@hotmail.com>
2018-04-29 15:18:53 +0100

    Add free controls interface and basic implementation

---
+++ include/open-sea/Controls.h
@@ -0,0 +1,74 @@
+/** \file Controls.h
+ * Components and systems related to entity controls
+ *
+ * \author Filip Smola
+ */
+#ifndef OPEN_SEA_CONTROLS_H
+#define OPEN_SEA_CONTROLS_H
+
+#include <open-sea/Controls.h>
+#include <open-sea/Log.h>
+#include <open-sea/Entity.h>
+#include <open-sea/Components.h>
+#include <open-sea/Input.h>
+
+namespace open_sea::controls {
+
+    /** \class FreeControls
+     * \brief Controls an entity using free controls
+     * Controls an entity using free controls.
+     * No restrictions on orientation or position control.
+     * Unified input control over X, Y, Z position and roll.
+     * Mouse control over pitch and yaw.
+     * Orientation control enabled by a key (as it requires the cursor to be disabled) - this includes roll control.
+     * Only one entity should be controlled at one time.
+     */
+    class FreeControls {
+        private:
+            //! Logger for this manager
+            log::severity_logger lg = log::get_logger("Camera Component Mgr");
+        public:
+            //! Transformation component manager
+            std::shared_ptr<ecs::TransformationComponent> transformMgr{};
+            //! Entity being controlled
+            ecs::Entity subject;
+            //! Key bindings and factors
+            struct Controls {
+                // Key bindings
+                input::unified_input forward;
+                input::unified_input backward;
+                input::unified_input left;
+                input::unified_input right;
+                input::unified_input up;
+                input::unified_input down;
+                //! Clockwise roll key
+                input::unified_input clockwise;
+                //! Counter clockwise roll key
+                input::unified_input counter_clockwise;
+                //! Key that turns orientation control on when held
+                input::unified_input turn;
+
+                // Factors
+                //! Left-right (strafing) speed in (units / second)
+                float speed_x;
+                //! Forward-backward speed (units / second)
+                float speed_z;
+                //! Up-down speed (units / second)
+                float speed_y;
+                //! Degrees turned per screen unit of mouse movement
+                float turn_rate;    //TODO: should probably be independent of the screen size (deg per screen fraction?)
+                //! Degrees per second
+                float roll_rate;
+            } controls;
+
+            FreeControls(std::shared_ptr<ecs::TransformationComponent> t, ecs::Entity s, Controls c);
+
+            void transform();
+            void setSubject(ecs::Entity newSubject);
+
+            void showDebug();
+    };
+
+};
+
+#endif //OPEN_SEA_CONTROLS_H
---
+++ src/Controls.cpp
@@ -0,0 +1,139 @@
+/*
+ * Control component and system implementations
+ */
+
+#include <open-sea/Controls.h>
+#include <open-sea/Delta.h>
+#include <open-sea/Window.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/quaternion.hpp>
+
+namespace open_sea::controls {
+
+    //--- start FreeControls implementation
+    /**
+     * \brief Construct the controls
+     * Constuct the controls assigning it a pointer to the relevant transformation component manager, subject and controls.
+     *
+     * \param t Transformation component manager
+     * \param s Subject
+     * \param c Controls
+     */
+    FreeControls::FreeControls(std::shared_ptr<ecs::TransformationComponent> t, ecs::Entity s,
+                               FreeControls::Controls c) : transformMgr(std::move(t)), subject(s), controls(c) {
+        //TODO: move to header?
+    }
+
+    /**
+     * \brief Transform the subject according to input
+     */
+    void FreeControls::transform() {
+        // Update position
+        glm::vec3 global_translate{};
+        {
+            int index = transformMgr->lookup(subject);
+            glm::vec3 local{};
+
+            // Gather input
+            if (input::is_held(controls.left))
+                local.x += -1;
+            if (input::is_held(controls.right))
+                local.x += 1;
+            if (input::is_held(controls.forward))
+                local.z += -1;
+            if (input::is_held(controls.backward))
+                local.z += 1;
+            if (input::is_held(controls.up))
+                local.y += 1;
+            if (input::is_held(controls.down))
+                local.y += -1;
+
+            // Normalise
+            float l = glm::length(local);
+            if (l != 0.0f && l != 1.0f)
+                local /= l;
+
+            // Transform and apply
+            global_translate = glm::rotate(transformMgr->data.orientation[index], local);
+            global_translate.x *= controls.speed_x;
+            global_translate.y *= controls.speed_y;
+            global_translate.z *= controls.speed_z;
+            global_translate *= time::get_delta();
+            transformMgr->translate(&index, &global_translate, 1);
+        }
+
+        // Update camera guide rotation
+        glm::quat rotation{};
+        if (input::is_held(controls.turn)) {
+            // Local axes of rotation
+            constexpr glm::vec3 pitch_axis{1.0f, 0.0f, 0.0f};   // Positive x axis
+            constexpr glm::vec3 yaw_axis{0.0f, 1.0f, 0.0f};     // Positive y axis
+            constexpr glm::vec3 roll_axis{0.0f, 0.0f, 1.0f};    // Positive z axis
+
+            // Ensure cursor is disabled
+            if (glfwGetInputMode(window::window, GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+                glfwSetInputMode(window::window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+            // Look up subject transformation
+            int index = transformMgr->lookup(subject);
+
+            // Retrieve delta
+            glm::vec2 cursor_delta = input::cursor_delta();
+
+            // Positive pitch is up
+            float pitch = cursor_delta.y * (- controls.turn_rate);
+
+            // Positive yaw is left
+            float yaw = cursor_delta.x * (- controls.turn_rate);
+
+            // Positive roll is counter clockwise
+            float roll = 0.0f;
+            if (input::is_held(controls.clockwise))
+                roll += controls.roll_rate;
+            if (input::is_held(controls.counter_clockwise))
+                roll -= controls.roll_rate;
+            roll *= time::get_delta();
+
+            // Compute transformation quaternion and transform
+            if (pitch != 0 || yaw != 0 || roll != 0) {
+                // Transform axes of rotation
+                glm::quat original = transformMgr->data.orientation[index];
+                glm::vec3 tr_pitch_ax = glm::rotate(original,  pitch_axis);
+                glm::vec3 tr_yaw_ax = glm::rotate(original, yaw_axis);
+                glm::vec3 tr_roll_ax = glm::rotate(original, roll_axis);
+
+                // Compute transformation
+                glm::quat pitchQ = glm::angleAxis(glm::radians(pitch), tr_pitch_ax);
+                glm::quat yawQ = glm::angleAxis(glm::radians(yaw), tr_yaw_ax);
+                glm::quat rollQ = glm::angleAxis(glm::radians(roll), tr_roll_ax);
+                rotation = rollQ * yawQ * pitchQ;
+
+                // Apply
+                transformMgr->rotate(&index, &rotation, 1);
+            }
+        } else {
+            // Ensure cursor is not disabled
+            if (glfwGetInputMode(window::window, GLFW_CURSOR) == GLFW_CURSOR_DISABLED)
+                glfwSetInputMode(window::window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+    }
+
+    /**
+     * \brief Set a new subject for this control
+     *
+     * \param newSubject New subject
+     */
+    void FreeControls::setSubject(ecs::Entity newSubject) {
+        subject = newSubject;
+    }
+
+    /**
+     * \brief Show ImGui debug information for this control
+     */
+    void FreeControls::showDebug() {
+        //TODO
+    }
+    //--- end FreeControls implementation
+
+}
